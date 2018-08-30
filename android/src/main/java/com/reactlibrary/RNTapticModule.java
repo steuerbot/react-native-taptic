47acf4b00572b17c487a97ad3ad5c4c4c7555491
johakr <Johannes.Kronmueller@gmail.com>
2018-08-30 08:05:19 +0200

    add android polyfill

--- android/src/main/java/com/reactlibrary/RNTapticModule.java
+++ android/src/main/java/com/reactlibrary/RNTapticModule.java
@@ -6,6 +6,10 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class RNTapticModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
@@ -18,5 +22,29 @@ public class RNTapticModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNTaptic";
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put("light", "");
+    constants.put("medium", "");
+    constants.put("heavy", "");
+    constants.put("success", "");
+    constants.put("warning", "");
+    constants.put("error", "");
+    return constants;
+  }
+
+  @ReactMethod
+  public void impact(String impactType) {
+  }
+
+  @ReactMethod
+  public void selection() {
+  }
+
+  @ReactMethod
+  public void notification(String notificationType) {
   }
 }
